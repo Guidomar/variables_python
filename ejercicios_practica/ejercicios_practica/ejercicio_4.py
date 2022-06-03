68c8faaa864d9800758b6109034ca5eaebbbf3c6
Guidomar <94272351+Guidomar@users.noreply.github.com>
2022-06-03 20:15:48 -0300

    Ejercicio de Práctica N°4 resuelto
    Las palabras ingresadas  las señalé como comentario en el código (#)

--- ejercicios_practica/ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicios_practica/ejercicio_4.py
@@ -11,16 +11,16 @@
 
 # Ingrese tres palabras y arme un acrónimo con ellas
 # Si desea puede modificar el código para ingresar más palabras
-print('Ingrese palabra 1:')
+print('Ingrese palabra 1:')  #Palabra_1: Yacimientos
 palabra_1 = str(input())
 
-print('Ingrese palabra 2:')
+print('Ingrese palabra 2:')   #Palabra-2: Petroliferos
 palabra_2 = str(input())
 
-print('Ingrese palabra 3:')
+print('Ingrese palabra 3:')   #Palabra_3: Fiscales
 palabra_3 = str(input())
 
 # De cada palabra debe tomar la primera letra y armar el acrónimo
 # Ejemplo: Alumbrado, barrido y limpieza --> ABL
 # Imprimir el resultado en pantalla
-print(palabra_1[0]+palabra_2[0]+palabra_3[0])
+print(palabra_1[0]+palabra_2[0]+palabra_3[0])   #Acrónimo: YPF
